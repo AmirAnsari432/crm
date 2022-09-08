8c07e758bfd5e11cfa5fe140712a8d190fac28f5
Amir Ansari <amirbhai432@gmail.com>
2022-09-09 00:31:42 +0530

    for live testing

--- src/Components/landinPage.jsx
+++ src/Components/landinPage.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LandinPage() {
   return (
-    <div className='d-flex justify-content-center bg-dark align-items-center vh-100'>
-        <div> 
-            <h1 className='text-white'>
-                Landing Page 
-            </h1>
-            <Link  to = '/products'> <button className="btn btn-primary">View Products </button>
-            </Link>
-        
-        </div>
+    <div className="d-flex justify-content-center bg-dark align-items-center vh-100">
+      <div>
+        <h1 className="text-white">Landing Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+          dignissimos nobis modi sed aliquam illo possimus molestiae incidunt
+          veniam. Et provident molestias voluptates consectetur impedit veniam
+          natus iusto dicta? Quis?
+        </p>
+        <Link to="/products">
+          {" "}
+          <button className="btn btn-primary">View Products </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default LandinPage;
